3c74312e6ebdc7e77aae123c8abfc9226e976d03
MiriYehezkel <miri.yehezkel@gmail.com>
2020-10-31 08:58:04 +0200

    add light brightness change state from boolean to int

--- Clean Code - TDD/christmas-lights-kata/christmas_lights/light_grid.py
+++ Clean Code - TDD/christmas-lights-kata/christmas_lights/light_grid.py
@@ -9,22 +9,24 @@ class Cell:
 
 
 class LightGrid:
+    TOGGLE_AMOUNT = 2
+
     def __init__(self, length: int, width: int):
         self._grid = self.__create_grid(length, width)
 
-    def __create_grid(self, length: int, width: int) -> List[List[bool]]:
+    def __create_grid(self, length: int, width: int) -> List[List[int]]:
         self._grid = []
         for i in range(length):
             self._grid.append([])
             for j in range(width):
-                self._grid[i].append(False)
+                self._grid[i].append(0)
         return self._grid
 
-    def cell(self, cell: Cell) -> bool:
+    def cell(self, cell: Cell) -> int:
         return self._grid[cell.i][cell.j]
 
     def turn_on(self, cell: Cell) -> None:
-        self._grid[cell.i][cell.j] = True
+        self._grid[cell.i][cell.j] += 1
 
     def turn_group_on(self, start_cell: Cell, end_cell: Cell) -> None:
         for curr_i in range(start_cell.i, end_cell.i + 1):
@@ -32,7 +34,8 @@ class LightGrid:
                 self.turn_on(Cell(curr_i, curr_j))
 
     def turn_off(self, cell: Cell) -> None:
-        self._grid[cell.i][cell.j] = False
+        if self.cell(cell) > 0:
+            self._grid[cell.i][cell.j] -= 1
 
     def turn_group_off(self, start_cell: Cell, end_cell: Cell) -> None:
         for curr_i in range(start_cell.i, end_cell.i + 1):
@@ -40,16 +43,22 @@ class LightGrid:
                 self.turn_off(Cell(curr_i, curr_j))
 
     def toggle(self, cell: Cell) -> None:
-        state = self._grid[cell.i][cell.j] is not True
-        self._grid[cell.i][cell.j] = state
+        self._grid[cell.i][cell.j] += self.TOGGLE_AMOUNT
 
     def toggle_group(self, start_cell: Cell, end_cell: Cell) -> None:
         for curr_i in range(start_cell.i, end_cell.i + 1):
             for curr_j in range(start_cell.j, end_cell.j + 1):
                 self.toggle(Cell(curr_i, curr_j))
 
+    def count_brightness(self) -> int:
+        count = 0
+        for i in range(len(self._grid)):
+            for j in range(len(self._grid[0])):
+                count += int(self._grid[i][j])
+        return count
+
     def print_grid(self) -> None:
         for i in range(len(self._grid)):
             for j in range(len(self._grid[0])):
-                print('*' if self._grid[i][j] else '_', end='  ')
+                print(self._grid[i][j], end='  ')
             print()
--- Clean Code - TDD/christmas-lights-kata/tests/test_light_grid.py
+++ Clean Code - TDD/christmas-lights-kata/tests/test_light_grid.py
@@ -3,44 +3,59 @@ import pytest
 from christmas_lights.light_grid import LightGrid, Cell
 
 
-def test_cell_turn_off():
+def test_cell_turn_on_and_off():
     g = LightGrid(1, 1)
     g.turn_on(Cell(0, 0))
-    assert g.cell(Cell(0, 0)) is True
+    assert g.cell(Cell(0, 0)) == 1
+    assert g.count_brightness() == 1
+
     g.turn_off(Cell(0, 0))
-    assert g.cell(Cell(0, 0)) is False
+    assert g.cell(Cell(0, 0)) == 0
+    assert g.count_brightness() == 0
 
 
-def test_cell_turn_group_off():
+def test_cell_turn_group_on_and_off():
     length = width = 3
     g = LightGrid(length, width)
     start_cell, end_cell = Cell(0, 0), Cell(2, 2)
     g.turn_group_on(start_cell, end_cell)
-    _assert_cells(g, start_cell, end_cell, True)
+    _assert_cells(g, start_cell, end_cell, 1)
+    assert g.count_brightness() == 9
+
     g.turn_group_off(start_cell, end_cell)
-    _assert_cells(g, start_cell, end_cell, False)
+    _assert_cells(g, start_cell, end_cell, 0)
+    assert g.count_brightness() == 0
+
+
+def test_minimal_brightness():
+    g = LightGrid(1, 1)
+    cell = Cell(0, 0)
+    assert g.cell(cell) == 0
+    assert g.count_brightness() == 0
+
+    g.turn_off(cell)
+    assert g.cell(cell) == 0
+    assert g.count_brightness() == 0
 
 
 def test_toggle():
     g = LightGrid(3, 3)
     cell = Cell(1, 2)
     g.toggle(cell)
-    assert g.cell(cell) is True
-    g.toggle(cell)
-    assert g.cell(cell) is False
+    assert g.cell(cell) is 2
+    assert g.count_brightness() == 2
 
 
 def test_toggle_group():
     g = LightGrid(3, 3)
     start_cell, end_cell = Cell(1, 2), Cell(2, 2)
     g.toggle_group(start_cell, end_cell)
-    _assert_cells(g, start_cell, end_cell, True)
-    g.toggle_group(start_cell, end_cell)
-    _assert_cells(g, start_cell, end_cell, False)
+    _assert_cells(g, start_cell, end_cell, 2)
+    assert g.count_brightness() == 4
 
 
-def _assert_cells(grid, start_cell, end_cell, expected_state):
+def _assert_cells(grid, start_cell, end_cell, expected_cell_state):
     for curr_i in range(start_cell.i, end_cell.i + 1):
         for curr_j in range(start_cell.j, end_cell.j + 1):
-            assert grid.cell(Cell(curr_i, curr_j)) is expected_state
+            assert grid.cell(Cell(curr_i, curr_j)) is expected_cell_state
 
